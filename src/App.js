c5274bdfdfe050d3f10d1c817e4e1d6938e28602
Beatriz Santos <86390538+BeatrizSanto@users.noreply.github.com>
2022-08-12 16:23:39 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class function1 extends Component {
+  render() {
+    return (
+      <div>
+        <p> Component class</p>
+      </div>
+    );
+  }
+}
